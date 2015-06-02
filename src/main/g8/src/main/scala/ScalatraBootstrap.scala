efd522e19eecb73aa33e433a93bddafaf9979549
changhai <changhai@baidu.com>
2015-06-02 11:23:31 +0800

    fixed build error

--- src/main/g8/src/main/scala/ScalatraBootstrap.scala
+++ src/main/g8/src/main/scala/ScalatraBootstrap.scala
@@ -6,7 +6,7 @@ class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
   	implicit val swagger = ResourceSwagger
     context.mount(new ResourcesApp, "/api-docs")
-    context.mount(new $servlet_name$Swagger, "/user/*")
-    context.mount(new $servlet_name$, "/*")
+    context.mount(new $name;format="Camel"$Swagger, "/user/*")
+    context.mount(new $name;format="Camel"$Servlet, "/*")
   }
 }
